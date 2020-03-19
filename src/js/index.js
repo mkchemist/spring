24b0b6e0f5707a512bdd5d85e221a148f5b20978
npmt-events <61161080+npmt-events@users.noreply.github.com>
2020-03-19 02:14:01 +0200

    Update index.js
    adding spring tab effect

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,28 @@
 import "../scss/index.scss";
+
+$(document).ready(function() {
+  // spring tabs
+  (function() {
+    let $tabs = $(".spring-tabs-container .spring-tab");
+    let $navs = $(".spring-tabs-container .spring-tabs-nav");
+    function initSpringTabs(index = 0) {
+      $navs.each(function(i, nav) {
+        $(nav).removeClass("spring-active-tab");
+      });
+      $tabs.each(function(i, tab) {
+        $(tab).css("display", "none");
+        $tabs.eq(index).css("display", "");
+        $navs.eq(index).addClass("spring-active-tab");
+      });
+    }
+    initSpringTabs();
+    $navs.each(function(i, _nav) {
+      let nav = $(_nav);
+      nav.click(function(e) {
+        e.preventDefault();
+        let index = nav.attr("data-index");
+        initSpringTabs(index);
+      });
+    });
+  })();
+});
